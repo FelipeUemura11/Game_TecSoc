c0d661377c67bc3f34184680845fa848db0a4d86
FelipeUemura11 <felipe.uemura11@gmail.com>
2025-04-15 19:20:36 -0300

    fase 1 - jogo teste

--- game_tecsoc/src/routes/index.tsx
+++ game_tecsoc/src/routes/index.tsx
@@ -3,13 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
-
+import Game from "../pages/Game";
 const AppRoutes:FC = () => {
 
     return(
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/game" element={<Game />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     );
